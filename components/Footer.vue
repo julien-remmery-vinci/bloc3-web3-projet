d7b623d61bcfe06a4fefb3cb727f3bad9a0b7880
julien-remmery-vinci <julien.remmery@student.vinci.be>
2024-11-11 21:52:45 +0100

    refactor: extract components from existing pages

---
+++ components/Footer.vue
@@ -0,0 +1,5 @@
+<template>
+    <footer class="site-footer">
+      <p>&copy; 2024 - Budget Planner Web 3</p>
+    </footer>
+</template>
---
+++ middleware/auth-redirect.global.js
@@ -0,0 +1,6 @@
+export default defineNuxtRouteMiddleware((to, from) => {
+  // TODO : redirect to login page if not authenticated
+  // if (to.path === "/") {
+  //   return navigateTo('/login');
+  // }
+})
