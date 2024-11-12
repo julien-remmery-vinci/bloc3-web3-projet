ec28b77383f13f05c8b857fad3d7573eb846dacc
julien-remmery-vinci <julien.remmery@student.vinci.be>
2024-11-12 16:05:46 +0100

    feat: initialize project structure and add essential configurations

--- components/Footer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-    <footer class="site-footer">
-      <p>&copy; 2024 - Budget Planner Web 3</p>
-    </footer>
-</template>
--- middleware/auth-redirect.global.js
+++
@@ -1,6 +0,0 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-  // TODO : redirect to login page if not authenticated
-  // if (to.path === "/") {
-  //   return navigateTo('/login');
-  // }
-})
