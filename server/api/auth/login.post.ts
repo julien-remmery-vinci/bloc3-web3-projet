fea1b5e4a4ce209bd6e3eac4ca6ebffea2cf1bb7
julien-remmery-vinci <julien.remmery@student.vinci.be>
2024-11-17 23:40:51 +0100

    feat: refactor Supabase session handling and remove unused components

--- server/api/auth/login.post.ts
+++
@@ -1,9 +0,0 @@
-import { serverSupabaseClient } from '#supabase/server'
-
-export default defineEventHandler(async (event) => {
-  const client = await serverSupabaseClient(event)
-
-  const user = await client.from('Users').select().single()
-  const body = await readBody(event)
-  return { body }
-})
